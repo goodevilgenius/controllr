e3d3c28ac70aa022294c7a886697bf6bda8aecfa
Dan Jones <good.evil.genius@gmail.com>
2015-12-27 14:14:01 -0600

    Added necessary actions

--- server/php/index.php
+++ server/php/index.php
@@ -6,9 +6,16 @@ $method = strtolower($_SERVER["REQUEST_METHOD"]);
 if ($method == "head") $method = "get";
 header("X-Requested-Method: $method");
 
-if (!file_exists("actions/" . $method . ".php")) {
+$pathparts = explode("/", $path);
+@list($empty, $device, $command, $id) = $pathparts;
+if (empty($device))  $device = "server";
+if (empty($command)) $command = "info";
+header("X-Requested-Device: $device");
+header("X-Requested-Command: $command");
+
+if (!file_exists("actions/{$method}-{$command}.php")) {
     header("HTTP/1.1 405 Method Not Allowed");
     die;
 }
 
-require_once("actions/" . $method . ".php");
+require_once("actions/{$method}-{$command}.php");
