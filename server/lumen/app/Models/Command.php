2b397bc926c5d9cb81d4f93a934f239aacb84cbe
Dan Jones <dan@danielrayjones.com>
2018-01-05 12:35:49 -0600

    💾 (server) add Command Model

---
+++ server/lumen/app/Models/Command.php
@@ -0,0 +1,43 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * A Receiver
+ */
+class Command extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'command',
+        'arguments',
+        'data',
+        'status',
+        'eta',
+        'return_code',
+        'output',
+    ];
+
+    /**
+     * The Receiver.
+     *
+     * @return HasOne
+     */
+    public function receiver(): HasOne
+    {
+        return $this->hasOne(Receiver::class);
+    }
+
+    /**
+     * The Sender.
+     *
+     * @return HasOne
+     */
+    public function sender(): HasOne
+    {
+        return $this->hasOne(Sender::class);
+    }
+}
