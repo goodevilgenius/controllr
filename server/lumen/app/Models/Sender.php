754b798d8be431276803eb114e8718d2f42471b6
Dan Jones <dan@danielrayjones.com>
2018-01-05 12:34:30 -0600

    💾 (server) Add Sender Model

---
+++ server/lumen/app/Models/Sender.php
@@ -0,0 +1,33 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * A Sender
+ */
+class Sender extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['slug'];
+
+    /**
+     * Sets slug.
+     *
+     * @param string $value
+     */
+    public function setSlugAttribute(string $value): void
+    {
+        $this->attributes['slug'] = str_slug($value);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function commands(): HasMany
+    {
+        return $this->hasMany(Command::class);
+    }
+}
