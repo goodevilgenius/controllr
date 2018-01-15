54a5f6bfc11968c926ef50bca1854cf2dc6889ef
Dan Jones <dan@danielrayjones.com>
2018-01-14 19:21:25 -0600

    💾 (server) Partially fill out CommandController #100DaysOfCode R1D13

--- server/lumen/app/Http/Controllers/CommandController.php
+++ server/lumen/app/Http/Controllers/CommandController.php
@@ -1,10 +1,15 @@
 <?php namespace App\Http\Controllers;
 
+use App\Models\Client;
+use App\Models\Command;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CommandController extends Controller
 {
     protected $request;
+    protected $client;
 
     /**
      * Constructor.
@@ -12,39 +17,94 @@ class CommandController extends Controller
     public function __construct(Request $request)
     {
         $this->request = $request;
+        $this->client = $this->request->user();
     }
 
-    public function index(string $name)
+    /**
+     * Checks that the request's client name matches the given name.
+     *
+     * For routes accessible by the receiver, check the name in the route matches.
+     * For routes accessible by the sender, check the name of the command's sender.
+     *
+     * @param string $name
+     */
+    protected function checkClient(string $name): void
     {
-        return "command list for $name";
+        if ($this->client->name != $name) abort(403);
     }
 
-    public function next(string $name)
+    /**
+     * Used by $name to get its own list of Commands
+     *
+     * @param  string $name
+     * @return Collection
+     */
+    public function index(string $name): Collection
     {
-        return "next command for $name";
+        $this->checkClient($name);
+        return $this->client->commands;
     }
 
-    public function store(string $name)
+    /**
+     * Used by $name to get the next command in its queue.
+     *
+     * @param  string $name
+     * @return Command
+     */
+    public function next(string $name): Command
     {
-        return "adding a command for $name";
+        $this->checkClient($name);
+        $command = $this->client->commands()->where(['status' => 'enqueued'])->first();
+        if (empty($command)) $command = $this->client->commands()->where('status', '!=', 'complete')->first();
+        return $command;
     }
 
-    public function show(string $name, int $id)
+    /**
+     * Adds a new command for $name
+     *
+     * @param  string $name
+     * @return Command
+     */
+    public function store(string $name): Command
     {
-        return "getting command $id for $name";
+        if ($this->client->type != 'sender') abort(403);
+        // @todo Add ability for receivers to specify allowed senders
+
+        $receiver = Client::where(['type' => 'receiver', 'name' => $name])->firstOrFail();
+        $command = $receiver->commands()->make($this->request->request->all());
+        $command->sender_id = $this->client->id;
+        $command->save();
+
+        return $command;
     }
 
-    public function replace(string $name, int $id)
+    /**
+     * Returns a command. This is used by the sender to check the status.
+     *
+     * @param  string $name
+     * @param  int $id
+     * @return Command
+     */
+    public function show(string $name, int $id): Command
+    {
+        if ($this->client->type != 'sender') abort(403);
+        $command = Command::findOrFail($id);
+        $this->checkClient($command->sender->name);
+
+        return $command;
+    }
+
+    public function replace(string $name, int $id): Command
     {
         return "replacing command $id for $name";
     }
 
-    public function destroy(string $name, int $id)
+    public function destroy(string $name, int $id): Response
     {
         return "deleting command $id for $name";
     }
 
-    public function update(string $name, int $id)
+    public function update(string $name, int $id): Command
     {
         return "$name is updating command $id";
     }
